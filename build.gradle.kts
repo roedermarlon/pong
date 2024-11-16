69367cf56992c3166ef58c311248bd7bf6f0fb2a
Marlon Röder <marlon.roeder@aol.com>
2024-11-16 15:34:31 +0100

    fix: not all files are commited from last commit
    Signed-off-by: Marlon Röder <marlon.roeder@aol.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,15 @@ plugins {
     id("java")
 }
 
-group = "de.marlonr"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = "de.marlonr"
+    version = "1.0"
+
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
+    }
+}
 
 repositories {
     mavenCentral()
@@ -11,24 +18,15 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("dev.httpmarco:osgan-netty:1.0.35-SNAPSHOT")
-    implementation("io.netty:netty5-common:5.0.0.Alpha5")
-    implementation("io.netty:netty5-transport:5.0.0.Alpha5")
 
     compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
 
     testCompileOnly("org.projectlombok:lombok:1.18.34")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
 
     // jetbrains annotations
     compileOnly("org.jetbrains:annotations:22.0.0")
-    testCompileOnly("org.jetbrains:annotations:22.0.0")
-
-    implementation("me.obsilabor:alert:1.0.8")
-    testImplementation("me.obsilabor:alert:1.0.8")
 }
 
 tasks.test {
--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,36 +2,14 @@ plugins {
     id("java")
 }
 
-group = "de.marlonr"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-    maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
-}
-
 dependencies {
     implementation(project(":"))
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("dev.httpmarco:osgan-netty:1.0.35-SNAPSHOT")
-    implementation("io.netty:netty5-common:5.0.0.Alpha5")
-    implementation("io.netty:netty5-transport:5.0.0.Alpha5")
+    implementation(libs.bundles.netty)
 
-    compileOnly("org.projectlombok:lombok:1.18.34")
-    annotationProcessor("org.projectlombok:lombok:1.18.34")
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
 
-    testCompileOnly("org.projectlombok:lombok:1.18.34")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
+    compileOnly(libs.annotations)
 
-    // jetbrains annotations
-    compileOnly("org.jetbrains:annotations:22.0.0")
-    testCompileOnly("org.jetbrains:annotations:22.0.0")
-
-    implementation("me.obsilabor:alert:1.0.8")
-    testImplementation("me.obsilabor:alert:1.0.8")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation(libs.alert)
 }
